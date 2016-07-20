ff8868e4ea047b0d38b7f4d11d7f8e491e1162ed
Burak Tokak <iletisim@buraktokak.com>
2016-07-20 18:45:37 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+var uygulama = angular.module("ngUygulamam", []);
+  uygulama.controller("ngKontrol", function($scope){
+    $scope.cumle = "";
+    $scope.imgRoot = "http://www.tdk.gov.tr/components/com_gts/assets/images/";
+    $scope.escHarf = function(hrf){
+      return turkishCase(hrf);
+    };
+  });
+
+  function turkishCase(hrf){
+    switch(hrf) {
+    case "ç" :
+    case "Ç" :
+        return "ct";
+        break;
+    case "ğ":
+    case "Ğ":
+        return "gt";
+        break;
+    case "ş":
+    case "Ş":
+        return "st";
+        break;
+    case "ö":
+    case "Ö":
+        return "öt";
+        break;
+    case "ü":
+    case "Ü":
+        return "ut";
+        break;
+    case "ı":
+    case "I":
+        return "it";
+        break;
+    case "İ":
+        return "i";
+        break;
+    default:
+        return hrf.toLowerCase();
+    }
+  }
